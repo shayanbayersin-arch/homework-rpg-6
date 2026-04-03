89ba735ef7873e0b77224c091d1f205347bff6cf
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-04-04 01:16:28 +0500

    feat: add the facade

---
+++ src/com/narxoz/rpg/facade/ArenaFacade.java
@@ -0,0 +1,10 @@
+package com.narxoz.rpg.facade;
+
+import com.narxoz.rpg.tournament.TournamentEngine;
+
+public class ArenaFacade {
+    public void start() {
+        System.out.println("Arena system ready...");
+
+    }
+}
