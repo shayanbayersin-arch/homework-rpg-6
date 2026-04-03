1559661a19f1b7b9f26ff49c4ada04809d5c0130
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-04-04 01:38:04 +0500

    feat: fixing the EnemySquad ArenaFacade problems

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -1,5 +1,7 @@
 package com.narxoz.rpg;
 
+import com.narxoz.rpg.facade.ArenaFacade;
+
 public class Main {
     public static void main(String[] args) {
         ArenaFacade game = new ArenaFacade();
